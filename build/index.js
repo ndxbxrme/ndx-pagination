71c6034682c0b639409ca55f08756b328ee5c8cd
ndxbxrme <lewis_the_cat@hotmail.com>
2017-05-03 21:35:50 +0100

    refactor, better handling of small lists and performance improvement

--- build/index.js
+++ build/index.js
@@ -15,7 +15,7 @@
     return {
       restrict: 'AE',
       require: 'ngModel',
-      template: "<div ng-show=\"totalPages &gt; 1\" class=\"pagination\"> <style type=\"text/css\">.pagination {display: flex;}.pagination .page {padding: 0.5rem;min-width: 1.2rem;cursor: pointer;user-select: none;}.pagination .page.selected {color: #f00;}.pagination .page.disabled {opacity: 0.5;pointer-events: none;}</style> <div ng-click=\"setPage(ngModel - 1)\" ng-class=\"{disabled:ngModel&lt;2}\" ng-hide=\"hidePrevNext\" class=\"page prev\">{{prevText}}</div> <div ng-click=\"setPage(1)\" ng-show=\"showFirstLast\" class=\"page first\">{{firstText}}</div> <div ng-show=\"ellipsisPre\" class=\"page ellipsis pre\">{{ellipsisText}}</div> <div ng-repeat=\"myPage in (allPages = getPages())\" ng-click=\"setPage(myPage)\" ng-class=\"{selected:ngModel===myPage}\" ng-show=\"shouldShowPage(myPage)\" class=\"page number\">{{myPage}}</div> <div ng-show=\"ellipsisPost\" class=\"page ellipsis post\">{{ellipsisText}}</div> <div ng-click=\"setPage(totalPages)\" ng-show=\"showFirstLast\" class=\"page last\">{{lastText}}</div> <div ng-click=\"setPage(ngModel + 1)\" ng-class=\"{disabled:ngModel&gt;totalPages-1}\" ng-hide=\"hidePrevNext\" class=\"page next\">{{nextText}}</div> </div>",
+      template: "<div ng-show=\"totalPages &gt; 1\" class=\"pagination\"> <style type=\"text/css\">.pagination {display: flex;}.pagination .page {padding: 0.5rem;min-width: 1.2rem;cursor: pointer;user-select: none;}.pagination .page.selected {color: #f00;}.pagination .page.disabled {opacity: 0.5;pointer-events: none;}</style> <div ng-click=\"setPage(ngModel - 1)\" ng-class=\"{disabled:ngModel&lt;2}\" ng-hide=\"hidePrevNext\" class=\"page prev\">{{prevText}}</div> <div ng-click=\"setPage(1)\" ng-show=\"showFirstLast\" class=\"page first\">{{firstText}}</div> <div ng-show=\"ellipsisPre\" class=\"page ellipsis pre\">{{ellipsisText}}</div> <div ng-repeat=\"myPage in (allPages = getPages())\" ng-click=\"setPage(myPage)\" ng-class=\"{selected:ngModel===myPage}\" class=\"page number\">{{myPage}}</div> <div ng-show=\"ellipsisPost\" class=\"page ellipsis post\">{{ellipsisText}}</div> <div ng-click=\"setPage(totalPages)\" ng-show=\"showFirstLast\" class=\"page last\">{{lastText}}</div> <div ng-click=\"setPage(ngModel + 1)\" ng-class=\"{disabled:ngModel&gt;totalPages-1}\" ng-hide=\"hidePrevNext\" class=\"page next\">{{nextText}}</div> </div>",
       replace: true,
       scope: {
         ngModel: '=',
@@ -35,37 +35,34 @@
       link: function(scope, elem) {
         scope.totalPages = 0;
         scope.getPages = function() {
-          var i, pages;
+          var end, i, pages, pagesToShow;
           scope.firstText = scope.firstText || 'First';
           scope.lastText = scope.lastText || 'Last';
           scope.prevText = scope.prevText || '<<';
           scope.nextText = scope.nextText || '>>';
           scope.ellipsisText = scope.ellipsisText || '..';
           pages = [];
-          i = 0;
+          pagesToShow = scope.pagesToShow || 5;
+          i = Math.max(0, scope.ngModel - Math.ceil(pagesToShow / 2));
+          end = i + pagesToShow;
           scope.totalPages = Math.ceil(scope.total / scope.pageSize);
-          while (i++ < scope.totalPages) {
+          if (end > scope.totalPages) {
+            end = scope.totalPages;
+            i = Math.max(0, end - pagesToShow);
+          }
+          console.log(i, end, scope.totalPages);
+          scope.ellipsisPre = false;
+          if (i > 0) {
+            scope.ellipsisPre = true;
+          }
+          scope.ellipsisPost = false;
+          if (i + pagesToShow < scope.totalPages) {
+            scope.ellipsisPost = true;
+          }
+          while (i++ < Math.min(scope.totalPages, end)) {
             pages.push(i);
           }
           return pages;
-        };
-        scope.shouldShowPage = function(page) {
-          var ceilHalf, floorHalf, pagesToShow;
-          pagesToShow = scope.pagesToShow || 5;
-          floorHalf = Math.floor(pagesToShow / 2);
-          ceilHalf = Math.ceil(pagesToShow / 2);
-          if (scope.showAllPages || Math.abs(page - scope.ngModel) < ceilHalf || (page < (pagesToShow + 1) && scope.ngModel < (pagesToShow - 1)) || (page > scope.totalPages - pagesToShow && scope.ngModel > scope.totalPages - ceilHalf)) {
-            return true;
-          }
-          scope.ellipsisPre = false;
-          scope.ellipsisPost = false;
-          if (scope.ngModel > ceilHalf) {
-            scope.ellipsisPre = true;
-          }
-          if (scope.ngModel < scope.totalPages - floorHalf && scope.totalPages > pagesToShow) {
-            scope.ellipsisPost = true;
-          }
-          return false;
         };
         return scope.setPage = function(page) {
           var base;
--- src/index.coffee
+++ src/index.coffee
@@ -14,7 +14,7 @@ module
       <div ng-click=\"setPage(ngModel - 1)\" ng-class=\"{disabled:ngModel&lt;2}\" ng-hide=\"hidePrevNext\" class=\"page prev\">{{prevText}}</div>
       <div ng-click=\"setPage(1)\" ng-show=\"showFirstLast\" class=\"page first\">{{firstText}}</div>
       <div ng-show=\"ellipsisPre\" class=\"page ellipsis pre\">{{ellipsisText}}</div>
-      <div ng-repeat=\"myPage in (allPages = getPages())\" ng-click=\"setPage(myPage)\" ng-class=\"{selected:ngModel===myPage}\" ng-show=\"shouldShowPage(myPage)\" class=\"page number\">{{myPage}}</div>
+      <div ng-repeat=\"myPage in (allPages = getPages())\" ng-click=\"setPage(myPage)\" ng-class=\"{selected:ngModel===myPage}\" class=\"page number\">{{myPage}}</div>
       <div ng-show=\"ellipsisPost\" class=\"page ellipsis post\">{{ellipsisText}}</div>
       <div ng-click=\"setPage(totalPages)\" ng-show=\"showFirstLast\" class=\"page last\">{{lastText}}</div>
       <div ng-click=\"setPage(ngModel + 1)\" ng-class=\"{disabled:ngModel&gt;totalPages-1}\" ng-hide=\"hidePrevNext\" class=\"page next\">{{nextText}}</div>
@@ -43,24 +43,23 @@ module
       scope.nextText = scope.nextText or '>>'
       scope.ellipsisText = scope.ellipsisText or '..'
       pages = []
-      i = 0
+      pagesToShow = scope.pagesToShow or 5
+      i = Math.max 0, scope.ngModel - Math.ceil(pagesToShow / 2)
+      end = i + pagesToShow
       scope.totalPages = Math.ceil scope.total / scope.pageSize
-      while i++ < scope.totalPages
+      if end > scope.totalPages
+        end = scope.totalPages
+        i = Math.max 0, end - pagesToShow
+      console.log i, end, scope.totalPages
+      scope.ellipsisPre = false
+      if i > 0
+        scope.ellipsisPre = true
+      scope.ellipsisPost = false
+      if i + pagesToShow < scope.totalPages
+        scope.ellipsisPost = true
+      while i++ < Math.min scope.totalPages, end
         pages.push i
       pages
-    scope.shouldShowPage = (page) ->
-      pagesToShow = scope.pagesToShow or 5
-      floorHalf = Math.floor pagesToShow / 2
-      ceilHalf = Math.ceil pagesToShow / 2
-      if scope.showAllPages or Math.abs(page - scope.ngModel) < ceilHalf or (page < (pagesToShow + 1) and scope.ngModel < (pagesToShow - 1)) or (page > scope.totalPages - pagesToShow and scope.ngModel > scope.totalPages - ceilHalf)
-        return true
-      scope.ellipsisPre = false
-      scope.ellipsisPost = false
-      if scope.ngModel > ceilHalf
-        scope.ellipsisPre = true
-      if scope.ngModel < scope.totalPages - floorHalf and scope.totalPages > pagesToShow
-        scope.ellipsisPost = true
-      false
     scope.setPage = (page) ->
       scope.ngModel = page
       scope.pageChange?()? page
